dbaf264499048bb84368d3205b31bf34d8a5d2f4
FFR66 <110553194+FFR66@users.noreply.github.com>
2022-10-15 21:17:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+function a() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/6iohu-31j9i.mp3";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function b() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/日语大师.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function c() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/你干嘛哎呦.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function d() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/我是你爸爸.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function e() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/哇哇的哇.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function f() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/你要结婚了.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
+function g() {
+  var audio = document.getElementById("music");
+  audio.src = "./录音/我爱的那个他.m4a";
+  if (audio.paused) {
+    audio.play(); //audio.play();// 播放
+  } else {
+    audio();
+  }
+}
